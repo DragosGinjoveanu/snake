b13e33a3457bfd9aa0f9a08e05c183d53b72d9b9
Dragos <ginjoveanu.dragos@gmail.com>
2021-08-17 15:33:35 +0300

    removed unnecessary code and modified variable declaration types

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //game board
 var table = [];
-for (var i = 1; i <= 9; i++) {
+for (let i = 1; i <= 9; i++) {
     table[i] = [];
-    for (var j = 1; j <= 9; j++) {
+    for (let j = 1; j <= 9; j++) {
         table[i][j] = 0;
     }
 }
@@ -30,11 +30,11 @@ function isInSnake(row, column) {
 
 //generates the next "apple" you need to eat
 function generateFood() {
-  var i = Math.floor(Math.random() * 9 + 1);
-  var j = Math.floor(Math.random() * 9 + 1);
+  const i = Math.floor(Math.random() * 9 + 1);
+  const j = Math.floor(Math.random() * 9 + 1);
   if (isInSnake(i, j) == 0) {
     table[i][j] = 2;
-    var id = i + " " + String(j);
+    const id = i + " " + String(j);
     document.getElementById(id).innerHTML = ('󠀠󠀠<i class="las la-apple-alt"></i>');
     document.getElementById(id).className = "btn btn-danger btn-lg"; 
   } else {
@@ -49,19 +49,19 @@ function lengthenSnake(id) {
 
 //generates snake + initial snake position
 function loadSnake() {
-  for (var i = 1; i <= 9; i++) {
+  for (let i = 1; i <= 9; i++) {
     $('#table').append(`
       <tr></tr>
-    `)
-    for (var j = 1; j <= 9; j++) {
+    `);
+    for (let j = 1; j <= 9; j++) {
       $('#table').append(`
         <td><button type="button" class="btn btn-secondary btn-lg" id = "` + i + " " + j +`"><i class="las la-stop"></i></button></td>
       `);
     }
   }
-  var i = 1;
-  for (var j = 1; j <= 3; j++) {
-    var id = i + " " + String(j);
+  let i = 1;
+  for (let j = 1; j <= 3; j++) {
+    const id = i + " " + String(j);
     lengthenSnake(id);
     snakeRow.push(i);
     snakeColumn.push(j);
@@ -71,16 +71,16 @@ function loadSnake() {
 }
 
 //moves snake's head
-function updateSnakeHead(row, column) {
+function updateSnakeHead() {
   score++;
   document.getElementById("score").innerHTML = "Score: " + score; 
   generateFood();
 }
 
 //moves snake's tail
-function updateSnakeTail(row, column) {
+function updateSnakeTail() {
   table[snakeRow[0]][snakeColumn[0]] = 0;
-  var id = snakeRow[0] + " " + String(snakeColumn[0]);
+  const id = snakeRow[0] + " " + String(snakeColumn[0]);
   document.getElementById(id).innerHTML = ('󠀠󠀠<i class="las la-stop"></i>');
   document.getElementById(id).className = "btn btn-secondary btn-lg"; 
   snakeRow.shift();
@@ -91,21 +91,21 @@ function updateSnakeTail(row, column) {
 function updateSnake(row, column, key) {
   snakeRow.push(row);
   snakeColumn.push(column);
-  var id = row + " " + String(column);
+  const id = row + " " + String(column);
   lengthenSnake(id);
   if (table[row][column] == 0) {
-    updateSnakeTail(row, column);
+    updateSnakeTail();
   } else if (table[row][column] == 2) {
-    updateSnakeHead(row, column);
+    updateSnakeHead();
   }
   table[row][column] = 1;
-  var arrow = {
+  const arrow = {
     ArrowUp: [row - 1, column],
     ArrowDown: [row + 1, column],
     ArrowLeft: [row, column - 1],
     ArrowRight: [row, column + 1]
   };
-  var cell = arrow[key];
+  const cell = arrow[key];
   timeout = setTimeout(function(){checkSnake(parseInt(cell[0]), parseInt(cell[cell.length - 1]), key)}, 500);
 }
 
@@ -126,10 +126,10 @@ function checkSnake(row, column, key) {
 
 //changes the snake's direction
 document.addEventListener('keyup', (event) => {
-    var headRow = snakeRow[snakeRow.length - 1];
-    var headColumn = snakeColumn[snakeColumn.length - 1];
+    const headRow = snakeRow[snakeRow.length - 1];
+    const headColumn = snakeColumn[snakeColumn.length - 1];
     clearTimeout(timeout);
-    var arrow = {
+    const arrow = {
       ArrowUp: [headRow - 1, headColumn],
       ArrowDown: [headRow + 1, headColumn],
       ArrowLeft: [headRow, headColumn - 1],
@@ -137,7 +137,7 @@ document.addEventListener('keyup', (event) => {
     };
     for (const [key, value] of Object.entries(arrow)) {
       if(event.key == `${key}`) {
-        var cell = `${value}`;
+        const cell = `${value}`;
         checkSnake(parseInt(cell[0]), parseInt(cell[cell.length - 1]), event.key);
       }
     }
